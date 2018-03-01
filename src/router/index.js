783aa6bdab93b7b2c97e945e8d07df79a11290a3
partnerccz <2779017796@qq.com>
2018-03-01 10:38:00 +0800

    修改添加了招聘页面等信息

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,10 @@ import Router from 'vue-router'
 import Index from '../components/Index'
 
 Vue.use(Router)
+const Invite = resolve => require(['../components/Invite'], resolve)
 
 export default new Router({
-  mode: 'hash',
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -14,6 +15,10 @@ export default new Router({
       meta: {
         title: '广州指猴互联网科技有限公司'
       }
+    }, {
+      path: '/invite',
+      name: 'Invite',
+      component: Invite
     }
   ]
 })
