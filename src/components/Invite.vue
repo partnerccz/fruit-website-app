ee0826d132c907b0271cefdeed565647e9e5ec27
partnerccz <2779017796@qq.com>
2018-03-01 20:37:26 +0800

    修改招聘页面的图片

--- src/components/Invite.vue
+++ src/components/Invite.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="invite-container">
     <img src="../images/invite2-1.jpg" width="100%" style="margin:0;padding: 0"/>
-    <img src="../images/invite1-4.jpg" width="100%" style="margin:-4px 0 0 0;padding: 0"/>
+    <img src="../images/invite1-5.jpg" width="100%" style="margin:-4px 0 0 0;padding: 0"/>
   </div>
 </template>
 
